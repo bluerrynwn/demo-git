4415937741fe00848ea333fdb93e208de82bb7a3
bluerrynwn <bluerrynwn@gmail.com>
2025-02-04 08:20:43 -0500

    initial commit

---
+++ src/test/java/pe/edu/idat/demo_git/DemoGitApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.demo_git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
